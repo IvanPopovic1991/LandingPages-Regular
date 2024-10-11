7e81913b0c4c0423f4a667531924e50352f33f4c
Vlajs <vladimirspasic1996@gmail.com>
2024-10-11 11:34:59 +0200

    - Created new method for full size screenshot. - Configured new testNg file for tests running but for all pages. - Runned all tests successfully and got full size screenshots.

--- src/test/java/TestsFortrade/BaseTestFortrade.java
+++ src/test/java/TestsFortrade/BaseTestFortrade.java
@@ -1,8 +1,13 @@
 package TestsFortrade;
 
+import Pages.BasePage;
 import Selenium_Core.DriverManager;
 import Selenium_Core.DriverManagerFactory;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+
+import java.awt.*;
+import java.io.IOException;
 
 public class BaseTestFortrade {
     WebDriver driver;
@@ -14,7 +19,9 @@ public class BaseTestFortrade {
         driver.get("https://www.fortrade.com/minilps/en/education/");
     }
 
-    public void baseTearDown() {
+    public void baseTearDown(String fileName) throws IOException, AWTException {
+        new BasePage(driver).takeScreenshot(fileName,driver.findElement(By.xpath("//div[@id='startTradingButton']")));
         driverManager.quitDriver();
+
     }
 }
--- src/test/java/TestsFortradeR/EducationLp.java
+++ src/test/java/TestsFortradeR/EducationLp.java
@@ -1,10 +1,12 @@
 package TestsFortradeR;
 import Pages.FortradeRPage;
-import com.github.javafaker.Faker;
 import faker.TestData;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+
+import java.awt.*;
+import java.io.IOException;
 
 
 public class EducationLp extends BaseTestFortradeR {
@@ -14,13 +16,13 @@ public class EducationLp extends BaseTestFortradeR {
         baseSetup("Chrome", "129");
     }
     @AfterMethod
-    public void tearDown(){
-        baseTearDown();
+    public void tearDown() throws IOException, AWTException {
+        baseTearDown("FortradeR page - Account registration");
     }
     @Test
     public void demoAccountRegistration(){
         FortradeRPage fortradeRPage = new FortradeRPage(driver);
         fortradeRPage.successfullyRegistration("Testq","Testa", TestData.emailGenerator(),"381", TestData.phoneNumberGenerator());
+        fortradeRPage.assertURL("https://ready.fortrade.com/#chartticket");
     }
-
 }
